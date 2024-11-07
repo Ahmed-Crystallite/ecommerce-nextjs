8622b6a86b23b4306cbb112220faf14c2abd9ffa
Ahmed-Crystallite3 <ahmed.r@crystallitedigital.com>
2024-11-07 05:29:14 +0500

    analytics added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,28 +1,34 @@
-import { Poppins } from "next/font/google";
-import "./globals.css";
-import { Footer, Header, Toaster } from "@/components";
-import { CartProvider } from "@/components/cart/CartContext";
+import { Analytics } from "@vercel/analytics/react"
+import { Poppins } from "next/font/google"
+import "./globals.css"
+import { Footer, Header, Toaster } from "@/components"
+import { CartProvider } from "@/components/cart/CartContext"
 
-const poppins = Poppins({ subsets: ["latin"], weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"] });
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+})
 
 export const metadata = {
   title: "Ecommerce Website",
-  description: "Discover a world of fashion, electronics, home goods, and more. Enjoy free shipping, easy returns, and top-notch customer service. Shop now!",
-};
+  description:
+    "Discover a world of fashion, electronics, home goods, and more. Enjoy free shipping, easy returns, and top-notch customer service. Shop now!",
+}
 
 export default function RootLayout({ children }) {
   return (
     <CartProvider>
-    <html lang="en">
-      <body className={poppins.className}>
-        <Header/>
-        <main>
-          {children}
-          <Toaster/>
-        </main>
-        <Footer/>
-      </body>
-    </html>
+      <html lang="en">
+        <body className={poppins.className}>
+          <Header />
+          <main>
+            {children}
+            <Analytics />
+            <Toaster />
+          </main>
+          <Footer />
+        </body>
+      </html>
     </CartProvider>
-  );
+  )
 }
